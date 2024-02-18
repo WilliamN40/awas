30cd1e338be0b265e72b0b0ee4074b97b6d501da
William <notowibowo40@gmail.com>
2024-02-18 12:45:13 +0700

    logo favicon

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
@@ -16,7 +17,10 @@ const Nav = () => {
             {/* Desktop */}
             <div className="mx-auto max-w-7xl flex h-16 items-center">
                 <div className="px-5 flex justify-between w-full md:w-auto flex-shrink-0">
-                    <Link href = '/'><h1 className='text-white'><b>AWAS</b>: Anti-Kejahatan dan Waspada </h1></Link>
+                    <Link href = '/'><div className="flex items-center gap-2">
+                        <Image src="/logo.png" alt="AWAS" width={40} height={40} />
+                        <h1 className='text-white'><b>AWAS</b>: Anti-Kejahatan dan Waspada </h1>
+                    </div></Link>
                     <button 
                         onClick={() => setMobileNavOpen(!mobileNavOpen)} 
                         className="md:hidden flex flex-col justify-center items-center"
